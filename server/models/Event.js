efee1252569352852a451d51848878b882a3d802
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-30 01:06:06 -0500

    feat: initial commit for server/models/Event.js

---
+++ server/models/Event.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Event', EventSchema);
